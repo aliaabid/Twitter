3336b964e1e5304b4dc7622238efbe7dd4c7e0b6
Aabid Ali <aabidali@Aabids-MacBook-Pro.local>
2019-02-26 18:47:07 -0600

    first commit

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Aabid Ali on 2/26/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
